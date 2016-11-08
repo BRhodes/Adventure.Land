0186e3e8a1913c5075be4de4302efba3cd0f7bfc
Brett Rhodes <BrettSquared@gmail.com>
2016-11-08 07:27:22 -0500

    tmp

---
+++ inventory.js
@@ -0,0 +1,18 @@
+class GearSafe() {
+  constructor() {
+
+  }
+
+
+}
+
+
+
+//inventory manager reserves inventory slots
+
+//crafting item manager manages crafting materials
+
+//gear swap managers gear from extra roles
+// - on load equip first set
+// - on gear add, move items into reserved region
+// - on swap, move gear from invo to equipped
