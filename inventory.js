9c59fa2f16d64fb19a00442b4aad1b905567140d
Brett Rhodes <BrettSquared@gmail.com>
2016-11-09 07:25:18 -0500

    tmp

--- inventory.js
+++ inventory.js
@@ -12,6 +12,32 @@ class GearSafe() {
 
 //crafting item manager manages crafting materials
 
+
+class Gear {
+  constructor() {
+    roles = [];
+    current_role = -1;
+  }
+
+  AddRole(role) {
+    roles.push(role);
+    if (roles.length == 1) {
+      current_role = 0;
+    }
+    OrganizeInv(role);
+    Switch(role);
+  }
+
+  OrganizeInv(role) {
+    for (let slot in role) {
+      for (let i = 0; i < 42; i++) {
+        if (IsUpgrade(character.items[i])) {
+
+        }
+      }
+    }
+  }
+}
 //gear swap managers gear from extra roles
 // - on load equip first set
 // - on gear add, move items into reserved region
